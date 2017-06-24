e105bbc4a2ca6f661d894b24b3db630234883ec1
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2017-06-24 20:00:53 +0300

    Make poster larger on brick wall

--- src/core/place-it-core.js
+++ src/core/place-it-core.js
@@ -10,7 +10,7 @@ function render(_opts) {
   }, _opts);
 
   const mapRenderOpts = _.omit(_.merge({}, opts, {
-    resizeToWidth: 400,
+    resizeToWidth: 800,
     resizeToHeight: null,
   }), _.isNil);
   return posterCore.render(mapRenderOpts)
