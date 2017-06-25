b98776d739f84b759f941a4c348788c9b3217778
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2017-06-25 15:00:00 +0300

    Add initial support for exact mockup photo placement

--- src/core/place-it-core.js
+++ src/core/place-it-core.js
@@ -4,22 +4,51 @@ const path = require('path');
 const posterCore = require('./poster-core');
 const sharp = require('sharp');
 
+const photoMetas = {
+  'brick-wall.jpg': {
+    type: 'center',
+    resizeToWidth: 800,
+  },
+  'white-frame-gold-hipster.jpg': {
+    type: 'exact',
+    size: '70x100cm',
+    topLeft: { x: 454, y: 180 },
+    topRight: { x: 996, y: 180 },
+    bottomRight: { x: 998, y: 956 },
+    bottomLeft: { x: 998, y: 956 },
+  },
+  'black-frame-pink-chair.jpg': {
+    type: 'exact',
+    size: '50x70cm',
+    topLeft: { x: 410, y: 85 },
+    topRight: { x: 907, y: 85 },
+    bottomRight: { x: 907, y: 776 },
+    bottomLeft: { x: 411, y: 777 },
+  },
+};
+
 function render(_opts) {
   const opts = _.merge({
     photo: 'brick-wall.jpg',
   }, _opts);
 
-  const mapRenderOpts = _.omit(_.merge({}, opts, {
-    resizeToWidth: 800,
-    resizeToHeight: null,
-  }), _.isNil);
-  return posterCore.render(mapRenderOpts)
-    .then(posterImage => BPromise.props({
-      photoImage: sharp(getFilePath(`./images/${opts.photo}`))
-        .overlayWith(posterImage)
-        .png()
-        .toBuffer(),
-    }))
+  const photoMeta = photoMetas[opts.photo];
+  if (!photoMeta) {
+    const err = new Error(`Photo not found: ${opts.photoMeta}`);
+    err.status = 404;
+    throw err;
+  }
+
+  let renderPromise;
+  if (photoMeta.type === 'center') {
+    renderPromise = _renderCenter(photoMeta, opts);
+  } else if (photoMeta.type === 'exact') {
+    renderPromise = _renderExact(photoMeta, opts);
+  } else {
+    throw new Error(`Unexpected photo type: ${photoMeta.type}`);
+  }
+
+  renderPromise
     .then(({ photoImage }) => {
       const photo = sharp(photoImage);
       if (_.isFinite(opts.resizeToWidth)) {
@@ -31,6 +60,42 @@ function render(_opts) {
       return photo;
     })
     .then(photo => photo.png().toBuffer());
+}
+
+function _renderExact(photoMeta, opts) {
+  const width = photoMeta.bottomRight.x - photoMeta.topLeft.x;
+  const mapRenderOpts = _.omit(_.merge({}, opts, {
+    size: photoMeta.size,
+    resizeToWidth: width,
+    resizeToHeight: null,
+  }), _.isNil);
+
+  return posterCore.render(mapRenderOpts)
+    .then(posterImage => BPromise.props({
+      photoImage: sharp(getFilePath(`./images/${opts.photo}`))
+        .overlayWith(posterImage, {
+          top: photoMeta.topLeft.y,
+          left: photoMeta.topLeft.x,
+          gravity: sharp.gravity.northwest,
+        })
+        .png()
+        .toBuffer(),
+    }));
+}
+
+function _renderCenter(photoMeta, opts) {
+  const mapRenderOpts = _.omit(_.merge({}, opts, {
+    resizeToWidth: photoMeta.resizeToWidth,
+    resizeToHeight: null,
+  }), _.isNil);
+
+  return posterCore.render(mapRenderOpts)
+    .then(posterImage => BPromise.props({
+      photoImage: sharp(getFilePath(`./images/${opts.photo}`))
+        .overlayWith(posterImage)
+        .png()
+        .toBuffer(),
+    }));
 }
 
 function getFilePath(relativePath) {
