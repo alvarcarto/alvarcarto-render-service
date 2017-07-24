16d64e505050a3ae8e7334b7e295c689196756d4
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2017-07-24 21:46:13 +0300

    Fix

--- src/core/raster-map-core-pool.js
+++ src/core/raster-map-core-pool.js
@@ -1,10 +1,9 @@
 const BPromise = require('bluebird');
 const path = require('path');
-const fs = require('fs');
 const glob = require('glob');
 const _ = require('lodash');
 const mapnik = require('mapnik');
-const mapnikPool = require('mapnik-pool')(mapnik);
+const AsyncLock = require('async-lock');
 const logger = require('../util/logger')(__filename);
 const config = require('../config');
 const rasterMapCore = require('./raster-map-core');
@@ -16,11 +15,14 @@ logger.info(`Preloading ${files.length} mapnik styles ..`);
 const mapnikCache = _.reduce(files, (memo, filePath) => {
   const styleName = path.basename(filePath, '.xml');
 
-  const pool = mapnikPool.fromString(fs.readFileSync(filePath, 'utf8'), { size: 256 }, {
-    base: config.STYLE_DIR,
-  });
+  const map = BPromise.promisifyAll(new mapnik.Map(100, 100));
+  map.loadSync(filePath, { strict: true });
+
   return _.extend({}, memo, {
-    [styleName]: BPromise.promisifyAll(pool),
+    [styleName]: {
+      map,
+      lock: new AsyncLock({ timeout: 10000, Promise: BPromise }),
+    },
   });
 }, {});
 
@@ -30,11 +32,12 @@ function render(_opts) {
   const opts = _.merge({}, _opts);
   const key = opts.mapStyle;
 
-  logger.info(`Aquiring a mapnik map from pool with key: ${key}`);
+  logger.info(`Aquiring a lock with key: ${key}`);
 
-  const pool = mapnikCache[key];
-  return pool.acquireAsync()
-    .then(map => rasterMapCore.render(_.merge({}, opts, {
+  const { map, lock } = mapnikCache[key];
+  return lock.acquire()
+    .tap(() => logger.info(`Got lock for key: ${key}`))
+    .then(() => rasterMapCore.render(_.merge({}, opts, {
       map,
     })));
 }
