6fed6b36df29061853ddf23b631c0d8d15c558fb
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2017-07-27 13:49:52 +0300

    Fix posters/dist cleaning

--- tools/build-posters.sh
+++ tools/build-posters.sh
@@ -7,7 +7,8 @@ echo -e "Optimizing ./posters/*.svg with svgo ..\n"
 ./node_modules/.bin/svgo --config=tools/svgo.yml -f ./posters
 
 echo -e "Removing old posters in posters/dist ..\n"
-rm -rf ./posters/dist/*
+rm -f ./posters/dist/*
+rm -f ./posters/dist/images/*
 
 echo -e "Running custom sanitize ..\n"
 node tools/sanitize-posters.js
