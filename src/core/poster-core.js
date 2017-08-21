5766434480d7ca2cb656ffb5eb741588c3b01d21
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2017-08-21 16:56:16 +0300

    Better support for custom poster sizes (#11)
    * Add better support for custom poster sizes
    
    * Fix custom file render
    
    * Support building custom posters
    
    * Fix custom sanitize
    
    * Fix custom sanitize
    
    * Fix custom sanitize
    
    * Fix custom sanitize
    
    * Fix custom sanitize
    
    * Fix custom sanitize
    
    * Add custom dir to gitkeep
    
    * Fix custom jsons

--- src/core/poster-core.js
+++ src/core/poster-core.js
@@ -248,7 +248,6 @@ function setTexts(svgDoc, opts) {
   const { labelColor } = getMapStyle(opts.mapStyle);
   const { addLines, upperCaseLabels } = getPosterStyle(opts.posterStyle);
 
-
   const labelHeader = upperCaseLabels
     ? opts.labelHeader.toUpperCase()
     : opts.labelHeader;
@@ -265,10 +264,14 @@ function setTexts(svgDoc, opts) {
     setColor(smallHeaderEl, labelColor);
 
     if (addLines) {
+      const strokeWidth = opts.custom
+        ? opts.custom.middleLineStrokeWidth
+        : posterSizeToMiddleLineStrokeWidth(opts.size);
+
       addOrUpdateLines(svgDoc, getSvgFromDocument(svgDoc), smallHeaderEl, {
         getBBoxForSvgElement: textEl => getBBoxForSvgElement(svgDocToStr(svgDoc), textEl.getAttribute('id')),
         svgAttributes: {
-          'stroke-width': posterSizeToMiddleLineStrokeWidth(opts.size),
+          'stroke-width': strokeWidth,
         },
         debugLines: config.DEBUG_POSTER_LINES,
       });
@@ -299,6 +302,10 @@ function parsePosterSvg(svgString) {
 }
 
 function readPosterFile(opts) {
+  if (opts.custom) {
+    return fs.readFileAsync(opts.custom.filePath, { encoding: 'utf8' });
+  }
+
   const serverFileName = `${opts.posterStyle}-${opts.size}-${opts.orientation}-server.svg`;
   const serverAbsPath = path.join(__dirname, '../../posters/dist', serverFileName);
   const clientFileName = `${opts.posterStyle}-${opts.size}-${opts.orientation}.svg`;
--- tools/build-posters.sh
+++ tools/build-posters.sh
@@ -6,12 +6,19 @@ set -x
 echo -e "Optimizing ./posters/*.svg with svgo ..\n"
 ./node_modules/.bin/svgo --config=tools/svgo.yml -f ./posters
 
+echo -e "Optimizing ./posters/custom/*.svg with svgo ..\n"
+./node_modules/.bin/svgo --config=tools/svgo.yml -f ./posters/custom
+
 echo -e "Removing old posters in posters/dist ..\n"
 rm -f ./posters/dist/*.svg
+rm -f ./posters/dist/custom/*.svg
 # Commented by default because it's faster to not download images on each
 # start
 # rm -f ./posters/dist/images/*
 rm -f ./*.png
+
+echo -e "Copying custom .json files ..\n"
+cp ./posters/custom/*.json ./posters/dist/custom/
 
 echo -e "Running custom sanitize ..\n"
 node tools/sanitize-posters.js
