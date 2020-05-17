225b2345411e2eb11fc8d92146e2981c30ae21d1
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2020-05-17 19:19:05 +0300

    Fix map pooling when used locally

--- src/core/map-core-pool.js
+++ src/core/map-core-pool.js
@@ -6,7 +6,11 @@ const _ = require('lodash');
 const mapnik = require('mapnik');
 const AsyncLock = require('async-lock');
 const logger = require('../util/logger')(__filename);
-const { replacePostgisParametersFileSync, AUTOGEN_SUFFIX } = require('../util/mapnik');
+const {
+  replacePostgisParametersFile,
+  replacePostgisParametersFileSync,
+  AUTOGEN_SUFFIX,
+ } = require('../util/mapnik');
 const config = require('../config');
 const rasterMapCore = require('./map-core');
 
@@ -24,14 +28,13 @@ if (cluster.isMaster) {
     const styleName = path.basename(filePath, '.xml');
 
     const map = BPromise.promisifyAll(new mapnik.Map(500, 500));
-
     const autogenStylePath = replacePostgisParametersFileSync(filePath);
     map.loadSync(autogenStylePath, { strict: true });
 
     return _.extend({}, memo, {
       [styleName]: {
         map,
-        lock: new AsyncLock({ timeout: 60000, Promise: BPromise }),
+        lock: createLock(),
       },
     });
   }, {});
@@ -39,22 +42,43 @@ if (cluster.isMaster) {
   logger.info('Mapnik styles loaded.');
 }
 
-function render(_opts) {
+function createLock() {
+  return new AsyncLock({ timeout: 60000, Promise: BPromise });
+}
+
+async function generateStyleLock(styleName) {
+  const styleFilePath = path.join(config.STYLE_DIR, `${styleName}.xml`);
+  const map = BPromise.promisifyAll(new mapnik.Map(500, 500));
+
+  const autogenStylePath = await replacePostgisParametersFile(styleFilePath);
+  await map.loadAsync(autogenStylePath, { strict: true });
+  return {
+    map,
+    lock: createLock(),
+  };
+}
+
+async function render(_opts) {
   const opts = _.merge({}, _opts);
   const key = opts.mapStyle;
 
   logger.info(`Aquiring a lock with key: ${key}`);
 
+  if (!_.has(mapnikCache, key)) {
+    logger.info(`Key not found in cache: ${key}, creating new`);
+    mapnikCache[key] = await generateStyleLock(key);
+  }
   const { map, lock } = mapnikCache[key];
-  return lock.acquire(key, () => {
+  const result = await lock.acquire(key, () => {
     logger.info(`Got lock for key: ${key}`);
 
     map.resize(opts.width, opts.height);
     return rasterMapCore.render(_.merge({}, opts, {
       map,
     }));
-  })
-  .tap(() => logger.info(`Released lock: ${key}`));
+  });
+  logger.info(`Released lock: ${key}`);
+  return result;
 }
 
 module.exports = {
